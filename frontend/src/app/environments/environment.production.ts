9ac57d4a3003b592f0d719864ecf00d30a4513e1
AndyDar92 <andydar20@gmail.com>
2024-04-17 21:04:14 -0500

    add workflow configuration to ng serve docker

--- frontend/src/app/environments/environment.production.ts
+++ frontend/src/app/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  API_MANAGER_TIME: 'http://localhost:3001/api',
+  production: true,
+  API_MANAGER_TIME: 'http://localhost:5001/api',
   API_ENV : '/env',
   API_TASK : '/task',
   API_LOGIN: '/security/login'
